c51e8b978a1f10dc88a23b6a6959ea56ec334898
socrates.apps.team <socrates.apps.team@gmail.com>
2022-11-09 01:11:52 +0700

    Tokopedia API SDK

--- src/main/java/com/bvk/partner/tokopedia/mitra/object/OrderSingle.java
+++ src/main/java/com/bvk/partner/tokopedia/mitra/object/OrderSingle.java
@@ -1,6 +1,5 @@
 package com.bvk.partner.tokopedia.mitra.object;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
--- src/main/java/com/bvk/partner/tokopedia/seller/object/Categories.java
+++ src/main/java/com/bvk/partner/tokopedia/seller/object/Categories.java
@@ -1,6 +1,5 @@
 package com.bvk.partner.tokopedia.seller.object;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Categories {
